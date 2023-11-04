c94a9b1a25a09eabfd0b6458b9e800c8c1bbef64
ehowey <ehowey@gmail.com>
2023-11-03 20:40:29 -0600

    WIP on demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <main
       className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] pb-24">
+      <div className="relative flex flex-col place-items-center">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert mb-4"
           src="/next.svg"
@@ -28,7 +28,7 @@ export default function HomePage() {
           priority
         />
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mt-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
           href="/bar-chart"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"

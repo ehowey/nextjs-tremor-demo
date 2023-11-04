6cd6c1a9ddea5f0e4456088bac8fc20d02fe53c6
ehowey <ehowey@gmail.com>
2023-11-03 20:33:40 -0600

    WIP on demo

---
+++ components/logos.tsx
@@ -0,0 +1,101 @@
+export const SunIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="5"></circle>
+      <line x1="12" y1="1" x2="12" y2="3"></line>
+      <line x1="12" y1="21" x2="12" y2="23"></line>
+      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+      <line x1="1" y1="12" x2="3" y2="12"></line>
+      <line x1="21" y1="12" x2="23" y2="12"></line>
+      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+    </svg>
+  );
+};
+
+export const CloudIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path>
+    </svg>
+  );
+};
+
+export const SnowIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M20 17.58A5 5 0 0 0 18 8h-1.26A8 8 0 1 0 4 16.25"></path>
+      <line x1="8" y1="16" x2="8.01" y2="16"></line>
+      <line x1="8" y1="20" x2="8.01" y2="20"></line>
+      <line x1="12" y1="18" x2="12.01" y2="18"></line>
+      <line x1="12" y1="22" x2="12.01" y2="22"></line>
+      <line x1="16" y1="16" x2="16.01" y2="16"></line>
+      <line x1="16" y1="20" x2="16.01" y2="20"></line>
+    </svg>
+  );
+};
+
+export const RainIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <line x1="16" y1="13" x2="16" y2="21"></line>
+      <line x1="8" y1="13" x2="8" y2="21"></line>
+      <line x1="12" y1="15" x2="12" y2="23"></line>
+      <path d="M20 16.58A5 5 0 0 0 18 7h-1.26A8 8 0 1 0 4 15.25"></path>
+    </svg>
+  );
+};
+
+export const FogIcon = (props: React.ComponentPropsWithoutRef<"svg">) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}
+    >
+      <path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2"></path>
+    </svg>
+  );
+};
--- pages/donut-chart.tsx
+++ pages/donut-chart.tsx
@@ -1,28 +1,88 @@
 import { DonutChart, Card } from "@tremor/react";
 import Link from "next/link";
+import { type TooltipProps } from "recharts";
+import type {
+  NameType,
+  ValueType,
+} from "recharts/types/component/DefaultTooltipContent";
+import {
+  CloudIcon,
+  FogIcon,
+  RainIcon,
+  SnowIcon,
+  SunIcon,
+} from "@/components/logos";
 
 const data = [
   {
     Weather: "Sunny",
     Classmates: 10,
+    icon: SunIcon,
   },
   {
     Weather: "Snowy",
     Classmates: 5,
+    icon: SnowIcon,
   },
   {
     Weather: "Cloudy",
     Classmates: 4,
+    icon: CloudIcon,
   },
   {
     Weather: "Rainy",
     Classmates: 2,
+    icon: RainIcon,
   },
   {
     Weather: "Foggy",
     Classmates: 1,
+    icon: FogIcon,
   },
 ];
+
+const customTooltip = ({
+  payload,
+  active,
+  label,
+}: TooltipProps<ValueType, NameType>) => {
+  if (!active || !payload) return null;
+  const Icon = payload[0].payload.icon;
+  const totalClassmates = data.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.Classmates,
+    0
+  );
+  return (
+    <div className="rounded-tremor-default text-tremor-default bg-tremor-background shadow-tremor-dropdown border-tremor-border dark:bg-dark-tremor-background dark:shadow-dark-tremor-dropdown dark:border-dark-tremor-border border">
+      <div className="border-tremor-border dark:border-dark-tremor-border inline-flex items-center border-b px-4 py-2">
+        <Icon className="mr-2 h-5 w-5" />
+        <p className="text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis font-medium">
+          {label}
+        </p>
+      </div>
+      <div className="space-y-1 px-4 py-2">
+        {payload.map((category, idx) => (
+          <div
+            className="flex items-center justify-between space-x-8"
+            key={idx}
+          >
+            <div className="flex items-center space-x-2">
+              <span
+                className={`rounded-tremor-full border-tremor-background shadow-tremor-card dark:border-dark-tremor-background dark:shadow-dark-tremor-card h-3 w-3 shrink-0 border-2 bg-${category.color}-500`}
+              ></span>
+              <p className="text-tremor-content dark:text-dark-tremor-content whitespace-nowrap text-right">
+                {category.dataKey}
+              </p>
+            </div>
+            <p className="text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis whitespace-nowrap text-right font-medium tabular-nums">
+              {category.value} / {totalClassmates}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const valueFormatter = (number: number) => {
   // Use reduce method to calculate total number of classmates
@@ -55,11 +115,11 @@ export default function BarChartPage() {
       <div className="mt-12">
         <Card>
           <DonutChart
+            variant="pie"
             data={data}
-            valueFormatter={valueFormatter}
+            customTooltip={customTooltip}
             index="Weather"
             category="Classmates"
-            variant="pie"
           />
         </Card>
       </div>
